3da2672ee87bc22ab4013dae5f3d7d019089e69e
Shubham Jadhav <jadhavshubham9718@gmail.com>
2026-08-08 22:11:26 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,24 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/minimax-game-ai/', 
-//   server: {
-//     host: '0.0.0.0', // Allow external access
-//     port: process.env.PORT || 3000, // Use Render's assigned port
-//   },
-//   preview: {
-//     host: '0.0.0.0',
-//     port: process.env.PORT || 3000,
-//   },
-// });
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: '/minimax-game-ai/', 
+  server: {
+    host: '0.0.0.0', // Allow external access
+    port: process.env.PORT || 3000, // Use Render's assigned port
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
+});
+
